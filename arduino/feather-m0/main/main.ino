93e569a146b745d9ece6cc8ce165f879cb316d13
Marc <marc@insightness.com>
2018-04-07 19:49:36 +0200

    first lcd test

--- arduino/feather-m0/main/main.ino
+++ arduino/feather-m0/main/main.ino
@@ -1,10 +1,60 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SharpMem.h>
+
+// any pins can be used
+#define SHARP_SCK  12
+#define SHARP_MOSI 11
+#define SHARP_SS   10
+
+// Set the size of the display here, e.g. 144x168!
+Adafruit_SharpMem display(SHARP_SCK, SHARP_MOSI, SHARP_SS, 144, 168);
+// The currently-available SHARP Memory Display (144x168 pixels)
+// requires > 4K of microcontroller RAM; it WILL NOT WORK on Arduino Uno
+// or other <4K "classic" devices!  The original display (96x96 pixels)
+// does work there, but is no longer produced.
+
+#define BLACK 0
+#define WHITE 1
+
+int minorHalfSize; // 1/2 of lesser of display width or height
+
 void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello!");
+
+  // start & clear the display
+  display.begin();
+  display.clearDisplay();
+
+  // Several shapes are drawn centered on the screen.  Calculate 1/2 of
+  // lesser of display width or height, this is used repeatedly later.
+  minorHalfSize = min(display.width(), display.height()) / 2;
+
+  // led
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  for(int i=0; i<4; i++) {
+    display.setRotation(i);
+    display.clearDisplay();
+    // text display tests
+    display.setTextSize(1);
+    display.setTextColor(BLACK);
+    display.setCursor(0,0);
+    display.println("Hello, world!");
+    display.setTextColor(WHITE, BLACK); // inverted text
+    display.println(3.141592);
+    display.setTextSize(2);
+    display.setTextColor(BLACK);
+    display.print("0x"); display.println(0xDEADBEEF, HEX);
+    // Screen must be refreshed at least once per second
+    for(int j=0; j<4; j++) {
+      display.refresh();
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(250);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(250);
+    }
+  }
 }
