604121a34f897d93848c8862f31c4e3ad8441901
Marc <marc@insightness.com>
2018-04-15 00:40:08 +0200

    added working wifi samples

--- arduino/feather-m0/main/main.ino
+++ arduino/feather-m0/main/main.ino
@@ -24,7 +24,7 @@ Adafruit_GPS GPS(&GPSSerial);
      
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
-#define GPSECHO false
+#define GPSECHO true
 
 uint32_t timer = millis();
 
